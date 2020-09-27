369674e900e99ccebca479646afadbc98b86ba8c
Lisias <me@lisias.net>
2020-09-27 20:34:53 -0300

    Adding source code for the Startup checks

---
+++ Source/TweakScaleCompanion_NF/Log.cs
@@ -0,0 +1,92 @@
+﻿/*
+	This file is part of TweakScaleCompanion_KIS
+	© 2020 LisiasT : http://lisias.net <support@lisias.net>
+
+	THIE FILE is licensed to you under:
+
+	* WTFPL - http://www.wtfpl.net
+		* Everyone is permitted to copy and distribute verbatim or modified
+ 		    copies of this license document, and changing it is allowed as long
+			as the name is changed.
+
+	THIE FILE is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+using System;
+using KSPe.Util.Log;
+using System.Diagnostics;
+
+#if DEBUG
+using System.Collections.Generic;
+#endif
+
+namespace TweakScaleCompanion.NF
+{
+	public static class Log
+	{
+		private static readonly Logger log = Logger.CreateForType<Startup>();
+
+		internal static void init()
+		{
+			log.level =
+#if DEBUG
+				Level.TRACE
+#else
+                Level.INFO
+#endif
+				;
+		}
+
+		internal static void force(string msg, params object[] @params)
+		{
+			log.force(msg, @params);
+		}
+
+		internal static void info(string msg, params object[] @params)
+		{
+			log.info(msg, @params);
+		}
+
+		internal static void warn(string msg, params object[] @params)
+		{
+			log.warn(msg, @params);
+		}
+
+		internal static void detail(string msg, params object[] @params)
+		{
+			log.detail(msg, @params);
+		}
+
+		internal static void error(Exception e, object offended)
+		{
+			log.error(offended, e);
+		}
+
+		internal static void error(string msg, params object[] @params)
+		{
+			log.error(msg, @params);
+		}
+
+		[ConditionalAttribute("DEBUG")]
+		internal static void dbg(string msg, params object[] @params)
+		{
+			log.trace(msg, @params);
+		}
+
+#if DEBUG
+		private static readonly HashSet<string> DBG_SET = new HashSet<string>();
+#endif
+
+		[ConditionalAttribute("DEBUG")]
+		internal static void dbgOnce(string msg, params object[] @params)
+		{
+			string new_msg = string.Format(msg, @params);
+#if DEBUG
+			if (DBG_SET.Contains(new_msg)) return;
+			DBG_SET.Add(new_msg);
+#endif
+			log.trace(new_msg);
+		}
+	}
+}
---
+++ Source/TweakScaleCompanion_NF/Startup.cs
@@ -0,0 +1,64 @@
+﻿/*
+	This file is part of TweakScaleCompanion_KIS
+	© 2020 LisiasT : http://lisias.net <support@lisias.net>
+
+	TweakScaleCompanion_KIS is double licensed, as follows:
+
+	* SKL 1.0 : https://ksp.lisias.net/SKL-1_0.txt
+	* GPL 2.0 : https://www.gnu.org/licenses/gpl-2.0.txt
+
+	And you are allowed to choose the License that better suit your needs.
+
+	TweakScaleCompanion_KIS is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+	You should have received a copy of the SKL Standard License 1.0
+	along with TweakScaleCompanion_KIS. If not, see <https://ksp.lisias.net/SKL-1_0.txt>.
+
+	You should have received a copy of the GNU General Public License 2.0
+	along with TweakScaleCompanion_KIS. If not, see <https://www.gnu.org/licenses/>.
+
+*/
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace TweakScaleCompanion.NF
+{
+	[KSPAddon(KSPAddon.Startup.Instantly, true)]
+	internal class Startup : MonoBehaviour
+	{
+		private void Start()
+		{
+			Log.init();
+			Log.force("Version {0}", Version.Text);
+
+			try
+			{
+				KSPe.Util.Installation.Check<Startup>(System.IO.Path.Combine("TweakScaleCompanion", "NF"));
+			}
+			catch (KSPe.Util.InstallmentException e)
+			{
+				Log.error(e.ToShortMessage());
+				KSPe.Common.Dialogs.ShowStopperAlertBox.Show(e);
+			}
+
+			this.checkDependencies();
+		}
+
+		private void checkDependencies()
+		{
+			try
+			{
+				AssemblyLoader.LoadedAssembly assembly = AssemblyLoader.loadedAssemblies.Where(a => a.assembly.GetName().Name == "Scale").First();
+				if (-1 == assembly.assembly.GetName().Version.CompareTo(new System.Version(2, 4, 4)) )
+					GUI.UnmetRequirementsShowStopperAlertBox.Show("TweakScale v2.4.4 or superior");
+			}
+			catch (Exception e) when (e is NullReferenceException || e is InvalidOperationException)
+			{
+				GUI.UnmetRequirementsShowStopperAlertBox.Show("TweakScale v2.4.4 or superior");
+			}
+		}
+	}
+}
